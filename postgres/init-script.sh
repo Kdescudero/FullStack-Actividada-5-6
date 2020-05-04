5b39b98633b8c4201d55df235b8332eacf8294a7
Kevin Escudero <kevin.escudero@ias.com.co>
2020-05-04 07:49:59 -0500

    Primer Commit

--- postgres/init-script.sh
+++ postgres/init-script.sh
@@ -4,34 +4,23 @@ set -e
 POSTGRES="psql --username ${POSTGRES_USER} -v ON_ERROR_STOP=1"
 
 $POSTGRES --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE orders;
+  CREATE DATABASE orders;
 EOSQL
 
 $POSTGRES --dbname "orders" <<-EOSQL
-    CREATE TABLE PRODUCTS(
-        id SERIAL PRIMARY KEY,
-        name VARCHAR NOT NULL,
-        description VARCHAR NOT NULL,
-        base_price DECIMAL NOT NULL,
-        tax_rate DECIMAL NOT NULL,
-        status VARCHAR NOT NULL,
-        inventory_quantity INT NOT NULL
-    );
-    CREATE COLLATION case_insensitive (provider = icu, locale = 'und-u-ks-level2', deterministic = false);
+  CREATE TABLE USERS(
+    ID SERIAL PRIMARY KEY,
+    USERNAME VARCHAR NOT NULL,
+    PASSWORD VARCHAR NOT NULL
+  );
 
-    create table users
-    (
-        username varchar(50) collate case_insensitive not null primary key,
-        password varchar(500)           not null,
-        enabled  boolean                not null
-    );
-
-    create table authorities
-    (
-        username  varchar(50) collate case_insensitive not null,
-        authority varchar(50) not null,
-        constraint fk_authorities_users foreign key (username) references users (username)
-    );
-
-    create unique index ix_auth_username on authorities(username, authority);
+  CREATE TABLE PRODUCTS(
+    ID SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    description VARCHAR NOT NULL,
+    basePrice DECIMAL NOT NULL,
+    taxRate VARCHAR NOT NULL,
+    productStatus VARCHAR NOT NULL,
+    inventoryQuantity INTEGER NOT NULL
+  );
 EOSQL
